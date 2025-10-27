0905b7eaa0f9120e5c2d2d7622ee7b260b4745bb
BlossomMarquez <marqb058@newschool.edu>
2025-10-27 17:08:47 -0400

    Pushing as a minimal viability project, still need to add tag sorting option, expand on info/introduction section, and include the 5 stories somewhere. may look into potential for other media types to be accepted e.g. pdfs, mp3s, etc...

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+// supabaseClient.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const supabaseUrl = 'https://saqodazywonhbxuxwfjm.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const SUPABASE_URL = 'https://saqodazywonhbxuxwfjm.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhcW9kYXp5d29uaGJ4dXh3ZmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNDI3NzQsImV4cCI6MjA3NjgxODc3NH0.2BWtPSaHCVMabhyEuAJ48brrhWOV5K-uyr7FZ8ivC1I'; // from Supabase dashboard
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
